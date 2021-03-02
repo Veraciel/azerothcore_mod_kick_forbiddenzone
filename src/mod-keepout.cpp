b5681a77c07e3fe7d58b933391fe36fd2384070d
Vitaliy <mitradis@gmail.com>
2021-03-02 12:53:46 +0300

    Update mod-keepout.cpp

--- src/mod-keepout.cpp
+++ src/mod-keepout.cpp
@@ -115,16 +115,6 @@ public:
     void OnBeforeConfigLoad(bool reload) override
     {
         if (!reload) {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/mod-keepout.conf";
-#ifdef WIN32
-            cfg_file = "mod-keepout.conf";
-#endif
-            std::string cfg_def_file = cfg_file + ".dist";
-
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-
-            sConfigMgr->LoadMore(cfg_file.c_str());
             maxwarnings = sConfigMgr->GetIntDefault("MaxWarnings", 3);
             KeepoutEnabled = sConfigMgr->GetBoolDefault("KeepOutEnabled", true);
 
